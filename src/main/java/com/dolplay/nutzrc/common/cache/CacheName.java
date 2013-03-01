f38ec8f922b0da7bb48de86009083a7924122309
Conanca <conanca2006@gmail.com>
2013-03-01 10:00:59 +0800

    提交CacheName类

--- src/main/java/com/dolplay/nutzrc/common/cache/CacheName.java
+++ src/main/java/com/dolplay/nutzrc/common/cache/CacheName.java
@@ -2,6 +2,7 @@ package com.dolplay.nutzrc.common.cache;
 
 public class CacheName {
 	public final static String SYSTEM_ALLUSERS = "cache:system:allusers";
+	public final static String SYSTEM_ALLUSERS_INPAGE = "cache:system:allusers:pager";
 	public final static String SYSTEM_USER = "cache:system:user";
 	public final static String SYSTEM_ALLUSERS_IDLIST = "cache:system:allusersidlist";
 	public final static String SYSTEM_OLDUSERS_IDLIST = "cache:system:oldusersidlist";
