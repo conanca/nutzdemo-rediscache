e0a87c56d1eb4752a6b8fe6ace7ffa49bb26294a
Conanca <conanca2006@gmail.com>
2013-03-01 10:45:44 +0800

    优化IocProvider

--- src/main/java/com/dolplay/nutzrc/common/IocProvider.java
+++ src/main/java/com/dolplay/nutzrc/common/IocProvider.java
@@ -4,15 +4,27 @@ import org.nutz.ioc.Ioc;
 import org.nutz.ioc.impl.NutIoc;
 import org.nutz.ioc.loader.combo.ComboIocLoader;
 
+/**
+ * TODO 实际项目中获取ioc的方式可灵活决定
+ * @author conanca
+ *
+ */
 public class IocProvider {
-	public static Ioc ioc() {
-		// TODO 实际项目中获取ioc的方式可灵活决定
-		Ioc ioc = null;
+
+	private static Ioc ioc;
+
+	public static void init() {
 		try {
 			ioc = new NutIoc(new ComboIocLoader("*org.nutz.ioc.loader.json.JsonLoader", "ioc.js", "dao.js", "jedis.js",
 					"*org.nutz.ioc.loader.annotation.AnnotationIocLoader", "com.dolplay.nutzrc"));
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
+		}
+	}
+
+	public static Ioc ioc() {
+		if (ioc == null) {
+			init();
 		}
 		return ioc;
 	}
