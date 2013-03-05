cddd5251cb08ea4071d4899cf08c7f4389b3ed04
Conanca <conanca2006@gmail.com>
2013-03-05 14:31:19 +0800

    重构缓存方法拦截器，复用大量代码

--- src/main/java/com/dolplay/nutzrc/common/cache/interceptor/AdvancedCacheInterceptor.java
+++ src/main/java/com/dolplay/nutzrc/common/cache/interceptor/AdvancedCacheInterceptor.java
@@ -1,154 +1,83 @@
 package com.dolplay.nutzrc.common.cache.interceptor;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.Date;
 import java.util.List;
 
 import org.nutz.aop.InterceptorChain;
-import org.nutz.aop.MethodInterceptor;
-import org.nutz.json.Json;
-import org.nutz.json.JsonFormat;
-import org.nutz.lang.Strings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.dolplay.nutzrc.common.cache.CStrings;
 import com.dolplay.nutzrc.common.cache.CacheType;
 import com.dolplay.nutzrc.common.cache.Order;
 import com.dolplay.nutzrc.common.cache.annotation.Cache;
-import com.dolplay.nutzrc.common.cache.annotation.CacheNameSuffix;
 import com.dolplay.nutzrc.common.cache.dao.AdvancedCacheDao;
 
 /**
  * @author Conanca
  * 实现缓存预先读取及缓存自动设值的方法拦截器，支持普通类型缓存和有序集型缓存(注意：有序集缓存都是永久缓存)
  */
-public class AdvancedCacheInterceptor implements MethodInterceptor {
+public class AdvancedCacheInterceptor extends CacheInterceptor {
 	private static Logger logger = LoggerFactory.getLogger(AdvancedCacheInterceptor.class);
 
 	private AdvancedCacheDao cacheDao;
 
-	public void filter(InterceptorChain chain) throws Throwable {
-		// 取得被拦截的方法及其注解
-		Method method = chain.getCallingMethod();
-		Cache cacheAn = method.getAnnotation(Cache.class);
-		// 获取缓存KEY的前缀：cacheNamePrefix
-		String cacheNamePrefix = cacheAn.cacheNamePrefix();
-		// 若cacheNamePrefix不为空，将对该缓存做相应操作否则直接执行方法
-		if (!Strings.isEmpty(cacheNamePrefix)) {
-			// 获取该方法欲读取的缓存的 KEY——将拼接方法注解中的cacheNamePrefix及标有“CacheNameSuffix”注解的参数
-			Object args[] = chain.getArgs();
-			String[] cacheParaArr = new String[args.length];
-			Annotation[][] ans = method.getParameterAnnotations();
-			int k = 0;
-			if (ans.length > 0) {
-				for (int i = 0; i < ans.length; i++) {
-					for (int j = 0; j < ans[i].length; j++) {
-						if (ans[i][j].annotationType() == CacheNameSuffix.class) {
-							if (args[i] == null) {
-								cacheParaArr[k] = "N/A";
-							} else if (CharSequence.class.isAssignableFrom(args[i].getClass())) {
-								cacheParaArr[k] = args[i].toString();
-							} else {
-								cacheParaArr[k] = Json.toJson(args[i], JsonFormat.compact().setQuoteName(false));
-							}
-							k++;
-						}
-					}
-				}
-			}
-			String cacheName = CStrings.cacheName(cacheNamePrefix, cacheParaArr);
-			logger.debug("Cache name : " + cacheName);
+	public AdvancedCacheDao cacheDao() {
+		return cacheDao;
+	}
 
-			// 获取缓存超时时间
-			int cacheTimeout = cacheAn.cacheTimeout();
-			// 获取缓存类型，根据缓存类型不同分别对缓存有不同的操作方式
-			CacheType cacheType = cacheAn.cacheType();
-			if (cacheType.equals(CacheType.String)) {
-				// 获取该方法欲读取的缓存的 VALUE
-				String cacheValue = null;
-				try {
-					cacheValue = cacheDao.get(cacheName);
-				} catch (Exception e) {
-					logger.error("Read Cache error", e);
-				}
-				// 若缓存值不为空，则该方法直接返回缓存里相应的值
-				if (cacheValue != null) {
-					chain.setReturnValue(Json.fromJson(method.getReturnType(), cacheValue));
-					logger.debug("Get a value from this cache");
-					return;
-				} else {
-					logger.debug("Can't get any value from this cache");
-				}
-				// 执行方法
-				chain.doChain();
-				// 获取方法返回值并增加相应缓存
-				Object returnObj = chain.getReturn();
-				if (returnObj != null) {
-					try {
-						//如果缓存超时时间设置的有效，则新增缓存时设置该超时时间，否则设置配置文件中所配置的超时时间
-						if (cacheTimeout > 0) {
-							cacheDao.set(cacheName, cacheTimeout, cacheValue);
-						} else {
-							cacheDao.set(cacheName, returnObj);
-						}
-						logger.debug("Set a new value for this cache");
-					} catch (Exception e) {
-						logger.error("Set cache error", e);
-					}
-				} else {
-					logger.warn("No value to set for this cache");
-				}
-			} else if (cacheType.equals(CacheType.List)) {
-				// 获取该方法欲读取的缓存的 VALUE
-				List<String> cacheValue = null;
-				try {
-					cacheValue = cacheDao.zQueryByRank(cacheName, 0, -1, Order.Asc);
-				} catch (Exception e) {
-					logger.error("Read Cache error", e);
-				}
-				// 若缓存值不为空，则该方法直接返回缓存里相应的值
-				if (cacheValue != null && cacheValue.size() > 0) {
-					chain.setReturnValue(cacheValue);
-					logger.debug("Get a value from this cache");
-					return;
-				} else {
-					logger.debug("Can't get any value from this cache");
-				}
-				// 执行方法
-				chain.doChain();
-				// 获取方法返回值并增加相应缓存
-				@SuppressWarnings("unchecked")
-				List<String> returnObj = (List<String>) chain.getReturn();
-				if (returnObj != null) {
-					try {
-						//如果缓存超时时间设置的有效，则新增缓存时设置该超时时间，否则设置配置文件中所配置的超时时间
-						if (cacheTimeout > 0) {
-							for (String item : returnObj) {
-								cacheDao.zAdd(cacheName, cacheTimeout, new Date().getTime(), item);
-								Thread.sleep(1);
-							}
-						} else {
-							for (String item : returnObj) {
-								cacheDao.zAdd(cacheName, new Date().getTime(), item);
-								Thread.sleep(1);
-							}
-						}
-						logger.debug("Set a new value for this cache");
-					} catch (Exception e) {
-						logger.error("Set cache error", e);
-					}
-				} else {
-					logger.warn("No value to set for this cache");
-				}
-			} else {
-				logger.error("The method annotation : CacheType Error!", new RuntimeException(
-						"The method annotation : CacheType Error"));
+	protected void cacheReturn(String cacheName, InterceptorChain chain, Method method, Cache cacheAn) throws Throwable {
+		// 获取缓存超时时间
+		int cacheTimeout = cacheAn.cacheTimeout();
+		// 获取缓存类型，根据缓存类型不同分别对缓存有不同的操作方式
+		CacheType cacheType = cacheAn.cacheType();
+		if (cacheType.equals(CacheType.String)) {
+			super.cacheReturn(cacheName, chain, method, cacheAn);
+		} else if (cacheType.equals(CacheType.List)) {
+			// 获取该方法欲读取的缓存的 VALUE
+			List<String> cacheValue = null;
+			try {
+				cacheValue = cacheDao().zQueryByRank(cacheName, 0, -1, Order.Asc);
+			} catch (Exception e) {
+				logger.error("Read Cache error", e);
 			}
-		} else {
+			// 若缓存值不为空，则该方法直接返回缓存里相应的值
+			if (cacheValue != null && cacheValue.size() > 0) {
+				chain.setReturnValue(cacheValue);
+				logger.debug("Get a value from this cache");
+				return;
+			} else {
+				logger.debug("Can't get any value from this cache");
+			}
 			// 执行方法
 			chain.doChain();
+			// 获取方法返回值并增加相应缓存
+			@SuppressWarnings("unchecked")
+			List<String> returnObj = (List<String>) chain.getReturn();
+			if (returnObj != null) {
+				try {
+					//如果缓存超时时间设置的有效，则新增缓存时设置该超时时间，否则设置配置文件中所配置的超时时间
+					if (cacheTimeout > 0) {
+						for (String item : returnObj) {
+							cacheDao().zAdd(cacheName, cacheTimeout, new Date().getTime(), item);
+							Thread.sleep(1);
+						}
+					} else {
+						for (String item : returnObj) {
+							cacheDao().zAdd(cacheName, new Date().getTime(), item);
+							Thread.sleep(1);
+						}
+					}
+					logger.debug("Set a new value for this cache");
+				} catch (Exception e) {
+					logger.error("Set cache error", e);
+				}
+			} else {
+				logger.warn("No value to set for this cache");
+			}
+		} else {
+			logger.error("The method annotation : CacheType Error!", new RuntimeException(
+					"The method annotation : CacheType Error"));
 		}
 	}
 }
--- src/main/java/com/dolplay/nutzrc/common/cache/interceptor/CacheInterceptor.java
+++ src/main/java/com/dolplay/nutzrc/common/cache/interceptor/CacheInterceptor.java
@@ -24,16 +24,39 @@ public class CacheInterceptor implements MethodInterceptor {
 
 	private CacheDao cacheDao;
 
+	public CacheDao cacheDao() {
+		return cacheDao;
+	}
+
 	public void filter(InterceptorChain chain) throws Throwable {
 		// 取得被拦截的方法及其注解
 		Method method = chain.getCallingMethod();
 		Cache cacheAn = method.getAnnotation(Cache.class);
+		Object[] args = chain.getArgs();
+		// 获取cacheName
+		String cacheName = createCacheName(args, method, cacheAn);
+		// 若cacheName不为空，将对该缓存及该方法返回值做相应操作否则直接执行方法
+		if (cacheName != null) {
+			cacheReturn(cacheName, chain, method, cacheAn);
+		} else {
+			// 执行方法
+			chain.doChain();
+		}
+	}
+
+	/**
+	 * 创建缓存名
+	 * @param chain
+	 * @return
+	 */
+	protected String createCacheName(Object args[], Method method, Cache cacheAn) {
+		String cacheName = null;
+
 		// 获取缓存KEY的前缀：cacheNamePrefix
 		String cacheNamePrefix = cacheAn.cacheNamePrefix();
 		// 若cacheNamePrefix不为空，将对该缓存做相应操作否则直接执行方法
 		if (!Strings.isEmpty(cacheNamePrefix)) {
 			// 获取该方法欲读取的缓存的 KEY——将拼接方法注解中的cacheNamePrefix及标有“CacheNameSuffix”注解的参数
-			Object args[] = chain.getArgs();
 			String[] cacheParaArr = new String[args.length];
 			Annotation[][] ans = method.getParameterAnnotations();
 			int k = 0;
@@ -53,48 +76,60 @@ public class CacheInterceptor implements MethodInterceptor {
 					}
 				}
 			}
-			String cacheName = CStrings.cacheName(cacheNamePrefix, cacheParaArr);
+			cacheName = CStrings.cacheName(cacheNamePrefix, cacheParaArr);
 			logger.debug("Cache name : " + cacheName);
+		} else {
+			logger.warn("cacheNamePrefix is empty!");
+		}
 
+		return cacheName;
+	}
+
+	/**
+	 * 操作缓存和方法的返回值
+	 * 先取缓存值，如果缓存值不为空，将其赋给方法的返回值；如果缓存值为空，将执行方法，并获取方法返回值，将该值再赋给缓存
+	 * @param cacheName
+	 * @param chain
+	 * @param method
+	 * @param cacheAn
+	 * @throws Throwable
+	 */
+	protected void cacheReturn(String cacheName, InterceptorChain chain, Method method, Cache cacheAn) throws Throwable {
+		// 获取该方法欲读取的缓存的 VALUE
+		String cacheValue = null;
+		try {
+			cacheValue = cacheDao().get(cacheName);
+		} catch (Exception e) {
+			logger.error("Read Cache error", e);
+		}
+		// 若缓存值不为空，则该方法直接返回缓存里相应的值
+		if (cacheValue != null) {
+			chain.setReturnValue(Json.fromJson(method.getReturnType(), cacheValue));
+			logger.debug("Get a value from this cache");
+			return;
+		} else {
+			logger.debug("Can't get any value from this cache");
+		}
+		// 执行方法
+		chain.doChain();
+		// 获取方法返回值并增加相应缓存
+		Object returnObj = chain.getReturn();
+		if (returnObj != null) {
 			// 获取缓存超时时间
 			int cacheTimeout = cacheAn.cacheTimeout();
-			// 获取该方法欲读取的缓存的 VALUE
-			String cacheValue = null;
 			try {
-				cacheValue = cacheDao.get(cacheName);
-			} catch (Exception e) {
-				logger.error("Read Cache error", e);
-			}
-			// 若缓存值不为空，则该方法直接返回缓存里相应的值
-			if (cacheValue != null) {
-				chain.setReturnValue(Json.fromJson(method.getReturnType(), cacheValue));
-				logger.debug("Get a value from this cache");
-				return;
-			} else {
-				logger.debug("Can't get any value from this cache");
-			}
-			// 执行方法
-			chain.doChain();
-			// 获取方法返回值并增加相应缓存
-			Object returnObj = chain.getReturn();
-			if (returnObj != null) {
-				try {
-					//如果缓存超时时间设置的有效，则新增缓存时设置该超时时间，否则设置配置文件中所配置的超时时间
-					if (cacheTimeout > 0) {
-						cacheDao.set(cacheName, cacheTimeout, cacheValue);
-					} else {
-						cacheDao.set(cacheName, returnObj);
-					}
-					logger.debug("Set a new value for this cache");
-				} catch (Exception e) {
-					logger.error("Set cache error", e);
+				//如果缓存超时时间设置的有效，则新增缓存时设置该超时时间，否则设置配置文件中所配置的超时时间
+				if (cacheTimeout > 0) {
+					cacheDao().set(cacheName, cacheTimeout, cacheValue);
+				} else {
+					cacheDao().set(cacheName, returnObj);
 				}
-			} else {
-				logger.warn("No value to set for this cache");
+				logger.debug("Set a new value for this cache");
+			} catch (Exception e) {
+				logger.error("Set cache error", e);
 			}
 		} else {
-			// 执行方法
-			chain.doChain();
+			logger.warn("No value to set for this cache");
 		}
 	}
 }
