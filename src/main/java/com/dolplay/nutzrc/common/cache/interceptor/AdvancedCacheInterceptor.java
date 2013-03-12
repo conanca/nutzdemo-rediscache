d4e64ae6ef805e993bf3ac4864c47512fe44d904
Conanca <conanca2006@gmail.com>
2013-03-12 16:08:13 +0800

    简化对list倒序的代码

--- src/main/java/com/dolplay/nutzrc/common/cache/interceptor/AdvancedCacheInterceptor.java
+++ src/main/java/com/dolplay/nutzrc/common/cache/interceptor/AdvancedCacheInterceptor.java
@@ -1,10 +1,10 @@
 package com.dolplay.nutzrc.common.cache.interceptor;
 
 import java.lang.reflect.Method;
+import java.util.Collections;
 import java.util.List;
 
 import org.nutz.aop.InterceptorChain;
-import org.nutz.lang.Lang;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -72,18 +72,17 @@ public class AdvancedCacheInterceptor extends CacheInterceptor {
 	}
 
 	private void setCache(String cacheKey, List<String> returnObj, boolean reverse, int cacheTimeout) throws Exception {
-		String[] itemArr = Lang.collection2array(returnObj);
 		if (reverse) {
-			Lang.reverse(itemArr);
+			Collections.reverse(returnObj);
 		}
 		//如果缓存超时时间设置的有效，则新增缓存时设置该超时时间，否则设置配置文件中所配置的超时时间
 		if (cacheTimeout != CacheConfig.INVALID_TIMEOUT) {
-			for (String item : itemArr) {
+			for (String item : returnObj) {
 				cacheDao().zAdd(cacheKey, cacheTimeout, System.currentTimeMillis(), item);
 				Thread.sleep(1);
 			}
 		} else {
-			for (String item : itemArr) {
+			for (String item : returnObj) {
 				cacheDao().zAdd(cacheKey, System.currentTimeMillis(), item);
 				Thread.sleep(1);
 			}
