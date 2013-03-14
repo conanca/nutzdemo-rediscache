ce8e0d16c84f5f22da1c7d952381a9bb0995f65a
Conanca <conanca2006@gmail.com>
2013-03-14 10:09:59 +0800

    修正static修饰符号顺序

--- src/main/java/com/dolplay/nutzrc/common/cache/CacheConfig.java
+++ src/main/java/com/dolplay/nutzrc/common/cache/CacheConfig.java
@@ -5,21 +5,21 @@ public class CacheConfig {
 	/**
 	 * 缓存名前缀和(一个或多个)后缀拼接时用的间隔符号 
 	 */
-	public final static String CACHEKEY_DELIMITER = ":";
+	public static final String CACHEKEY_DELIMITER = ":";
 
 	/**
 	 * 默认的普通类型缓存的超时时间，单位为秒
 	 */
-	public final static int DEFAULT_STANDARD_CACHE_TIMEOUT = 600;
+	public static final int DEFAULT_STANDARD_CACHE_TIMEOUT = 600;
 
 	/**
 	 * 默认的有序集合类型缓存的超时时间，单位为秒
 	 */
-	public final static int DEFAULT_LIST_CACHE_TIMEOUT = 3600;
+	public static final int DEFAULT_LIST_CACHE_TIMEOUT = 3600;
 
 	/**
 	 * 用于指明Cache注解中cacheTimeout的特定标志值，表示使用配置文件所配置的超时时间
 	 */
-	public final static int INVALID_TIMEOUT = -9527;
+	public static final int INVALID_TIMEOUT = -9527;
 
 }
--- src/main/java/com/dolplay/nutzrc/common/cache/CacheKeyPrefix.java
+++ src/main/java/com/dolplay/nutzrc/common/cache/CacheKeyPrefix.java
@@ -1,9 +1,9 @@
 package com.dolplay.nutzrc.common.cache;
 
 public class CacheKeyPrefix {
-	public final static String SYSTEM_ALLUSERS = "cache:system:allusers";
-	public final static String SYSTEM_ALLUSERS_INPAGE = "cache:system:allusers:pager";
-	public final static String SYSTEM_USER = "cache:system:user";
-	public final static String SYSTEM_ALLUSERS_IDLIST = "cache:system:allusersidlist";
-	public final static String SYSTEM_OLDUSERS_IDLIST = "cache:system:oldusersidlist";
+	public static final String SYSTEM_ALLUSERS = "cache:system:allusers";
+	public static final String SYSTEM_ALLUSERS_INPAGE = "cache:system:allusers:pager";
+	public static final String SYSTEM_USER = "cache:system:user";
+	public static final String SYSTEM_ALLUSERS_IDLIST = "cache:system:allusersidlist";
+	public static final String SYSTEM_OLDUSERS_IDLIST = "cache:system:oldusersidlist";
 }
