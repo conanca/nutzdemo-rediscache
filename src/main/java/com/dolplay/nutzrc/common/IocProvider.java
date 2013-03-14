b61163979957aa4d151a6aa1be5f803a2a39dd30
Conanca <conanca2006@gmail.com>
2013-03-14 10:13:37 +0800

    增加log

--- src/main/java/com/dolplay/nutzrc/common/IocProvider.java
+++ src/main/java/com/dolplay/nutzrc/common/IocProvider.java
@@ -3,6 +3,8 @@ package com.dolplay.nutzrc.common;
 import org.nutz.ioc.Ioc;
 import org.nutz.ioc.impl.NutIoc;
 import org.nutz.ioc.loader.combo.ComboIocLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * TODO 实际项目中获取ioc的方式可灵活决定
@@ -10,6 +12,7 @@ import org.nutz.ioc.loader.combo.ComboIocLoader;
  *
  */
 public class IocProvider {
+	private static Logger logger = LoggerFactory.getLogger(IocProvider.class);
 
 	private static Ioc ioc;
 
@@ -18,7 +21,7 @@ public class IocProvider {
 			ioc = new NutIoc(new ComboIocLoader("*org.nutz.ioc.loader.json.JsonLoader", "ioc.js", "dao.js", "jedis.js",
 					"*org.nutz.ioc.loader.annotation.AnnotationIocLoader", "com.dolplay.nutzrc"));
 		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
+			logger.error("Ioc create error", e);
 		}
 	}
 
